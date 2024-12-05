6411bf2884a4c6eaa0a005449261589730a8bae7
medayoubadri <medayoubadri@gmail.com>
2024-12-05 22:30:34 +0100

    Task 14: Write a function that measures the balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -29,8 +29,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
+	if (left_height >= right_height)
+		return (1 + left_height);
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+	return (1 + right_height);
 }
 
 /**
@@ -46,8 +48,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left_height = tree->left ? (int)binary_tree_height(tree->left) : -1;
-	right_height = tree->right ? (int)binary_tree_height(tree->right) : -1;
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	return (left_height - right_height);
 }
